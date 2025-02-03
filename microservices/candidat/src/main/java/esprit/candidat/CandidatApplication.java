3d62da8237884f5f8284733dff3a733b9064c24b
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2025-02-03 15:43:01 +0100

    first step: crud candidat

--- microservices/candidat/src/main/java/esprit/candidat/CandidatApplication.java
+++ microservices/candidat/src/main/java/esprit/candidat/CandidatApplication.java
@@ -1,7 +1,10 @@
 package esprit.candidat;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CandidatApplication {
@@ -9,5 +12,12 @@ public class CandidatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CandidatApplication.class, args);
 	}
-
+    @Autowired
+	private CandidatRepository candidatRepository;
+	@Bean
+	public ApplicationRunner init() {
+		return (args) -> {
+			candidatRepository.save(new Candidat("Badia","Bouhdid","badia@gmail.com"));
+		};
+}
 }
---
+++ microservices/candidat/src/main/java/esprit/candidat/CandidatRestApi.java
@@ -0,0 +1,13 @@
+package esprit.candidat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CandidatRestApi {
+    //simple web service for testing
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
