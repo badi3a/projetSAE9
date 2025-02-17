449d48e82be38a649abf77ecd32fd56388279fde
dorraElamri <dorra.elamri@esprit.tn>
2025-02-17 13:08:19 +0100

    Initial commit

--- microservices/job/src/main/java/esprit/job/repositorys/CategorieRepository.java
+++
@@ -1,7 +0,0 @@
-package esprit.job.repositorys;
-
-import esprit.job.entities.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-}
